fd7cdd27ee56911d40177032ee089c9d09220f7e
Ouail Amarir <ouailamarir@gmail.com>
2025-02-09 18:51:32 +0100

    Amélioration du script de pré-commit pour vérifier la validité de la signature du dernier commit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# Vérifie si le dernier commit est signé
+# Vérifie si le dernier commit est signé et valide
 if ! git log -1 --pretty=%G? | grep -q "G"; then
-    echo "❌ Erreur : Le commit n'est pas signé !"
+    echo "❌ Erreur : Le commit n'est pas signé ou la signature n'est pas valide !"
     echo "✅ Veuillez signer votre commit avec 'git commit -S -m \"message\"'."
-    exit 1
-fi
-
-# Vérifie si la signature est valide
-if ! git log -1 --pretty=%G? | grep -q "G"; then
-    echo "❌ Erreur : La signature du commit n'est pas valide !"
     exit 1
 fi
 
