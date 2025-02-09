e83331728b8d85992eb754b617d9a60d65eb0b96
Ouail Amarir <ouailamarir@gmail.com>
2025-02-09 13:27:18 +0100

    Ajout d'un workflow de linter et d'un script de pré-commit pour vérifier la signature des commits

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Vérifie si le dernier commit est signé
+if ! git log -1 --pretty=%G? | grep -q "G"; then
+    echo "❌ Erreur : Le commit n'est pas signé !"
+    echo "✅ Veuillez signer votre commit avec 'git commit -S -m \"message\"'."
+    exit 1
+fi
+
+echo "✅ Commit signé, vous pouvez pousser votre code."
+exit 0
